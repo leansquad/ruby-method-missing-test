4a3af61fe8655ef0b317c7d2a9145add1f5b141e
Denis Blokhin <paskudeidb@gmail.com>
2021-05-07 18:27:36 +0300

    Revert "Fix class logic"
    This reverts commit 9875b4df8ffc66bf056e88af405716a97bf3aa97.

--- json_db.rb
+++ json_db.rb
@@ -1,6 +1,4 @@
 require 'json'
-require 'active_support/inflector'
-
 class JsonDb
   
   def initialize(json_filename)
@@ -20,32 +18,8 @@ class JsonDb
     IO.write(json_filename, data.to_json)
   end
 
-  def method_missing(method_name, *_args)
-    value = data[method_name.to_s]
-
-    if value.is_a? Array
-      value.map { |params| @class ||= ActiveSupport::Inflector.safe_constantize(ActiveSupport::Inflector.singularize(method_name).capitalize).new(params: params) }
-    else
-      value
-    end
+  def method_missing(method_name, *args, &block)
+    data[method_name.to_s]
   end
-end
-
-class Employee
-  attr_accessor :first_name, :last_name
-
-  def initialize(params:)
-    @first_name = params['first_name']
-    @last_name = params['last_name']
-  end
-end
-
-class Company
-  attr_accessor :name, :location, :employees
-
-  def initialize(params:)
-    @name = params['name']
-    @location = params['location']
-    @employees = params['employees'].map { |employee| Employee.new(params: employee) }
-  end
+  
 end
